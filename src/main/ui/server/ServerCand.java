93f585aec1f8075c273d8f639d686970e6c95e89
Bearsi89 <123056076+Bearsi89@users.noreply.github.com>
2023-02-12 13:56:45 +0900

    2월 12일 ServerCand GUI 추가

--- src/main/ui/server/ServerCand.java
+++ src/main/ui/server/ServerCand.java
@@ -1,7 +1,94 @@
 package main.ui.server;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 // 서버 후보 추가
-public class ServerCand extends JPanel {
+public class ServerCand extends JPanel implements ActionListener {
+	
+	private JLabel lblTitle;
+	private JLabel lbl01,lbl02;
+	private JLabel lblcombo;
+	
+	private JButton btnCreate, btnEdit, btnDelete, btnCancel;
+	
+	private String[] tableName = {"기호", "이름", "나이", "사진등록여부"};
+	private String[] comboName = {"선거1", "선거2", "선거3"};
+	private JComboBox<String> cbElec = new JComboBox(comboName);
+	
+	DefaultTableModel dt = new DefaultTableModel(tableName, 0);
+	JTable jt = new JTable(dt);
+	JScrollPane jsp = new JScrollPane(jt);
+	
+	JPanel panelMain;
+	JPanel panel01,panel02,panel03;
+	
+	ServerCand(){
+		
+		lblTitle = new JLabel("후보 관리");
+		lblTitle.setHorizontalAlignment(JLabel.CENTER);
+		lblTitle.setFont(new Font("Serif",Font.BOLD,30));
+		
+		lblcombo = new JLabel("선거 종류 : ");
+		
+		lbl01 = new JLabel();
+		lbl02 = new JLabel();
+		
+		btnCreate = new JButton("추 가");
+		btnEdit = new JButton("수 정");
+		btnDelete = new JButton("삭 제");
+		btnCancel = new JButton("나가기");
+		
+		panelMain = new JPanel();
+		panelMain.setLayout(new BorderLayout(10,10));
+		panel01 = new JPanel();
+		panel01.setLayout(new BorderLayout());
+		panel02 = new JPanel();
+		panel02.setLayout(new GridLayout(6, 2,10,40));
+		panel03 = new JPanel();
+		
+		panel03.add(lblcombo);
+		panel03.add(cbElec);
+		
+		panel01.add(lblTitle, BorderLayout.NORTH);
+		panel01.add(jsp, BorderLayout.CENTER);
+		
+		
+		panel02.add(panel03);
+		panel02.add(btnCreate);
+		panel02.add(btnEdit);
+		panel02.add(btnDelete);
+		panel02.add(btnCancel);
+		panel02.add(lbl02);
+		
+		panelMain.add(panel01, BorderLayout.CENTER);
+		panelMain.add(panel02, BorderLayout.EAST);
+		
+		add(panelMain);
+		
+		btnCreate.addActionListener(this);
+		btnEdit.addActionListener(this);
+		btnDelete.addActionListener(this);
+		btnCancel.addActionListener(this);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == btnCreate) {
+			new UserJDialogGUI(this, "추 가");
+		}else if(e.getSource() == btnEdit) {
+			new UserJDialogGUI(this, "수  정");
+		}else if(e.getSource() == btnDelete) {
+			UserJDialogGUI.massageConfirmBox(this, "정말 삭제하시겠습니까?");
+		}else if(e.getSource() == btnCancel) {
+			
+		}
+	}
 }
--- src/main/ui/server/UserJDialogGUI.java
+++ src/main/ui/server/UserJDialogGUI.java
@@ -11,43 +11,77 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class UserJDialogGUI extends JDialog {
-	
-	JPanel pLabel = new JPanel(new GridLayout(2,1,5,10));
-	JPanel pField = new JPanel(new GridLayout(2,1,5,10));
+
+	JPanel pLabel;
+	JPanel pField;
 	JPanel pButton = new JPanel();
-	
+
 	JLabel lblName = new JLabel("이  름");
+	JLabel lblAge = new JLabel("나 이");
 	JLabel lblImg = new JLabel("사  진");
-	
+
 	public JTextField txtName = new JTextField(10);
+	public JTextField txtAge = new JTextField(10);
 	public JTextField txtImg = new JTextField(10);
-	
+
 	JButton btnOK = new JButton("확인");
 	JButton btnCancel = new JButton("취소");
-	
+
 	public UserJDialogGUI(ServerElec sElec, String name) {
-		setTitle("추가/수정");
+		setTitle("선거 추가/수정");
 		
+		pLabel = new JPanel(new GridLayout(2,1,5,10));
+		pField = new JPanel(new GridLayout(2,1,5,10));
+
 		pLabel.add(lblName);
 		pLabel.add(lblImg);
-		
+
 		pField.add(txtName);
 		pField.add(txtImg);
-		
+
 		pButton.add(btnOK);
 		pButton.add(btnCancel);
-		
+
 		add(pLabel, "West");
 		add(pField, "Center");
 		add(pButton, "South");
-		
+
 		setSize(300,150);
 		setVisible(true);
-		
+
 		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
+	}//생성자
+
+	public UserJDialogGUI(ServerCand serverCand, String name) {
+
+		setTitle("후보자 추가/수정");
 		
-	}
-	
+		pLabel = new JPanel(new GridLayout(3,1,5,10));
+		pField = new JPanel(new GridLayout(3,1,5,10));
+
+		pLabel.add(lblName);
+		pLabel.add(lblAge);
+		pLabel.add(lblImg);
+
+		pField.add(txtName);
+		pField.add(txtAge);
+		pField.add(txtImg);
+
+		pButton.add(btnOK);
+		pButton.add(btnCancel);
+
+		add(pLabel, "West");
+		add(pField, "Center");
+		add(pButton, "South");
+
+		setSize(300,200);
+		setVisible(true);
+
+		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+
+	}//생성자
+
 	public static void messageBox(Object obj, String message) {
 		JOptionPane.showMessageDialog((Component)obj, message);
 	}
@@ -58,6 +92,6 @@ public class UserJDialogGUI extends JDialog {
 
 
 
-	
+
 
 }
